243e59a87e9823535f8cf7d70d31dc86ce218130
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2013-08-02 16:44:44 +0400

    Function dumptime has been deleted. Use asctime

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ static void
 show(char *zone, struct tm *tmp)
 {
     (void) printf("%s = ", zone);
-    dumptime(tmp);
+    printf("%s", asctime(tmp));
     if (tmp != NULL) {
         if (*abbr(tmp) != '\0')
             (void) printf(" %s", abbr(tmp));
@@ -57,55 +57,3 @@ abbr(struct tm *tmp)
     result = tzname[tmp->tm_isdst];
     return (result == NULL) ? &nada : result;
 }
-
-static void
-dumptime(register const struct tm *timeptr)
-{
-    static const char   wday_name[][3] = {
-        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
-    };
-    static const char   mon_name[][3] = {
-        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    };
-    register const char *   wn;
-    register const char *   mn;
-    register int        lead;
-    register int        trail;
-
-    if (timeptr == NULL) {
-        (void) printf("NULL");
-        return;
-    }
-    if (timeptr->tm_wday < 0 || timeptr->tm_wday >=
-            (int) (sizeof wday_name / sizeof wday_name[0]))
-        wn = "???";
-    else        wn = wday_name[timeptr->tm_wday];
-    if (timeptr->tm_mon < 0 || timeptr->tm_mon >=
-            (int) (sizeof mon_name / sizeof mon_name[0]))
-        mn = "???";
-    else        mn = mon_name[timeptr->tm_mon];
-    (void) printf("%.3s %.3s%3d %.2d:%.2d:%.2d ",
-                  wn, mn,
-                  timeptr->tm_mday, timeptr->tm_hour,
-                  timeptr->tm_min, timeptr->tm_sec);
-#define DIVISOR 10
-#ifndef TM_YEAR_BASE
-#define TM_YEAR_BASE    1900
-#endif /* !defined TM_YEAR_BASE */
-    trail = timeptr->tm_year % DIVISOR + TM_YEAR_BASE % DIVISOR;
-    lead = timeptr->tm_year / DIVISOR + TM_YEAR_BASE / DIVISOR +
-            trail / DIVISOR;
-    trail %= DIVISOR;
-    if (trail < 0 && lead > 0) {
-        trail += DIVISOR;
-        --lead;
-    } else if (lead < 0 && trail > 0) {
-        trail -= DIVISOR;
-        ++lead;
-    }
-    if (lead == 0)
-        (void) printf("%d", trail);
-    else    (void) printf("%d%d", lead, ((trail < 0) ? -trail : trail));
-}
-
